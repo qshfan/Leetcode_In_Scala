23099c4e9b100f590c90fe79f0b1cb388f4860df
qshfan <qiusha.fan@gmail.com>
2020-12-15 14:46:59 +0100

    Filter Array

--- src/hreasy/FilterArray.scala
+++ src/hreasy/FilterArray.scala
@@ -1,5 +1,20 @@
 package hreasy
 
-object FilterArray {
+object FilterArray extends App {
+  def f(delim:Int,arr:List[Int]):List[Int] = for {
+    elem <- arr if elem < delim
+  } yield elem
+
+  def f2(delim:Int,arr:List[Int]):List[Int] = {
+    arr.filter(e => e < delim)
+  }
+
+  def f3(delim:Int,arr:List[Int]):List[Int] = {
+    arr.filter(_ < delim)
+  }
+  val testList = List(1,2,3,4,4,4,4,5,5,5)
+  println(f(4,testList))
+  println(f2(4,testList))
 
 }
+
